b552adba2bd4b06544119b4f230e5001ad2b8448
tzetzo <tzvetanmarinov@yahoo.com>
2018-08-24 21:27:46 +0300

    test

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const env = process.env.NODE_ENV || 'development';  //heroku will set NODE_ENV otherwise we set it locally
+const env = process.env.NODE_ENV || 'development';  //heroku sets it to 'production'; package.json sets it to 'test' if the test script is executed
 console.log('env *****', env);
-if (env === 'development') {
-  process.env.PORT = 8080;
-}
-
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
-const port = process.env.PORT; //process.env.PORT is provided by heroku
+const port = process.env.PORT || 8080; //process.env.PORT is provided by heroku
 const app = express();
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname)); //provides a static website with bundle.js containing all JS code build by webpack
+
+
+app.get('/node', (req, res) => {
+  res.send({node: env});
+});
 
 app.get('*', (req, res) => { //sends the user index.html by default
   res.sendFile(path.resolve(__dirname, 'index.html'));
