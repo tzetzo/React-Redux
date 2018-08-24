40cff397dbfa36fa39239738e70b42669722cf8b
tzetzo <tzvetanmarinov@yahoo.com>
2018-08-24 19:15:17 +0300

    CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+var cors = require('cors');
 
 const port = process.env.PORT || 8080; //process.env.PORT is provided by heroku
 const app = express();
 
 app.use(express.static(__dirname)); //provides a static website with bundle.js containing all JS code build by webpack
+app.use(cors())
 
 app.get('*', (req, res) => { //sends the user index.html by default
   res.sendFile(path.resolve(__dirname, 'index.html'));
