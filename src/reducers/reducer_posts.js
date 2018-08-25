cb9cb1da31cc1ede62c0c8416eb16be2902659fb
tzetzo <tzvetanmarinov@yahoo.com>
2018-08-25 10:16:24 +0300

    bug fix

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,10 +5,13 @@ export default function(state = {}, action) {
   switch (action.type) {
     case DELETE_POST:
       return _.omit(state, action.payload);  //returns new state without the post with the given key/id
+      break;
     case FETCH_POST: //if user already visited PostsIndex component we dont want to lose the data;
-      return { ...state, [action.payload._id]: action.payload.data }  //the [] are necessary like in obj[id]
+      return { ...state, [action.payload.data._id]: action.payload.data };  //the [] are necessary like in obj[id]
+      break;
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, '_id');  //converting the array of objects into an object with the keys equal to the ids of the objects and the values the objects themselves
+      break;
     default:
       return state;
   }
